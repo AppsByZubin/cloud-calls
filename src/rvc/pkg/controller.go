eb7832eeccbf25bc46655f51ae323dca16125b55
amit <zubin.amit@gmail.com>
2021-11-24 02:18:53 -0800

    changed the structure depending on req

---
+++ src/rvc/pkg/controller.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("PA Controller")
+	fmt.Println("PA1 Controller")
+	fmt.Println("PA2 Controller")
+	fmt.Println("PA3 Controller")
+}
---
+++ src/sa/pkg/controller.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("SA Controller")
+	fmt.Println("SA1 Controller")
+	fmt.Println("SA2 Controller")
+	fmt.Println("SA3 Controller")
+}
