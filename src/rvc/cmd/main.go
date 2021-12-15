ade7dac8b75f6846ca16fdf0232151a909958da2
amit <zubin.amit@gmail.com>
2021-12-15 00:24:37 -0800

    commit-1 15th Dec

--- src/rvc/cmd/main.go
+++ src/rvc/cmd/main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("RVC")
 	fmt.Println("RVC1")
 	fmt.Println("RVC2")
+
+	fmt.Println("RVC3")
+	fmt.Println("RVC4")
+	fmt.Println("RVC5")
 }
--- src/rvc/pkg/controller.go
+++ src/rvc/pkg/controller.go
@@ -7,4 +7,10 @@ func main() {
 	fmt.Println("PA1 Controller")
 	fmt.Println("PA2 Controller")
 	fmt.Println("PA3 Controller")
+
+	fmt.Println("PA4 Controller")
+	fmt.Println("PA5 Controller")
+	fmt.Println("PA6 Controller")
+	fmt.Println("PA7 Controller")
+
 }
